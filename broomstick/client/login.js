a0bdbb89b372368cd14882a80b9f7fc7a2258d31
Isabel Luk <isabelluk@Isabels-MacBook-Pro.local>
2016-11-26 09:52:07 +0200

    WIP login system

---
+++ broomstick/client/login.js
@@ -0,0 +1,17 @@
+Template.login.events({
+	'submit form': function(event){
+		event.preventDefault();
+		var usernameVar = event.target.txt_username.value;
+		var passwordVar  = event.target.txt_password.value;
+		console.log("Form submitted.");
+		Meteor.loginWithPassword(usernameVar, passwordVar, function(err) {
+			if (err) {
+				Session.set('errorMessage', err.message);
+			}
+		});
+	}
+
+	errorMessage: function() {
+		return Session.get('errorMessage');
+	}
+});
