47a8766c5f3f5526c265d22670b1071db22efddf
Steven <steven.chien@connect.polyu.hk>
2016-11-26 20:21:48 +0100

    working version of desk management

--- broomstick/client/desk.js
+++ broomstick/client/desk.js
@@ -1,10 +1,36 @@
-Template.desk.helpers({
-	freeDeskCount: function() {
-		var userId = Meteor.userId();
-		var freeDesks;
-		if(userId) {
-			freeDesks = Desks.find({ value: 0 }).count();
-			return freeDesks;
+function draw() {
+	canvas = document.getElementById('myCanvas');
+	ctx = canvas.getContext('2d');
+	var img = new Image();
+	img.src = "img/floor_plan.png";
+
+	// Make sure the image is loaded first otherwise nothing will draw.
+	img.onload = function(){
+		try {
+			ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
 		}
-	},
+		catch(err) {
+			console.log(err);
+		}
+		finally {
+			Desks.find({ value: 0 }).forEach(function(desk) {
+				ctx.fillRect(desk.x/72.53*600-1,desk.y/72.53*600-1,5,5);
+			});
+		}
+	}
+}
+
+Template.desk.onRendered(function() {
+	draw();
+});
+
+Template.desk.helpers({
+	'emptyDesks': function() {
+		var userId = Meteor.userId();
+		//if(userId) {
+			var x = Desks.find({ value: 0 }).count();
+			draw();
+			return x;
+		//}
+	}
 });
